4c28f4be3be81556bd0d26f8a69086b397e03121
dfauser <dominikfauser@web.de>
2020-04-03 07:24:08 +0200

    commit on03/04/2020 at 07:24:08

--- visco_elastic/hyperelastic.py
+++ visco_elastic/hyperelastic.py
@@ -1,9 +1,25 @@
 from fenics import *
 from ufl import nabla_div
+from ufl import nabla_grad
 # Scaled variables
 from dolfin import *
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+from  scipy.integrate import quad
 
+
+plt.rcParams['axes.facecolor']='lightsteelblue'
+#mpl.rcParams['text.usetex'] = True
+#mpl.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}'] 
+plt.rc('lines',linewidth=1,linestyle='solid')
+plt.rc('font',size=18)
+plt.rc('grid',color='w')
+mpl.rcParams['axes.grid'] = True
+mpl.rcParams['xtick.color']='grey'
+mpl.rcParams['ytick.color']='grey'
+plt.rcParams['axes.labelcolor']='darkblue'
+mpl.rcParams["axes.edgecolor"] = 'w'
 # Optimization options for the form compiler
 parameters["form_compiler"]["cpp_optimize"] = True
 ffc_options = {"optimize": True, \
@@ -14,8 +30,6 @@ ffc_options = {"optimize": True, \
 # Create mesh and define function space
 mesh = UnitCubeMesh(24, 16, 16)
 V = VectorFunctionSpace(mesh, "Lagrange", 1)
-plot(mesh)
-plt.savefig('mesh_hyper.pdf')
 # Mark boundary subdomians
 left =  CompiledSubDomain("near(x[0], side) && on_boundary", side = 0.0)
 right = CompiledSubDomain("near(x[0], side) && on_boundary", side = 1.0)
@@ -25,11 +39,21 @@ right = CompiledSubDomain("near(x[0], side) && on_boundary", side = 1.0)
 def neo_hook_energy(mu,lmbda,Ic,J):
 	return (mu/2)*(Ic - 3) - mu*ln(J) + (lmbda/2)*(ln(J))**2
 
-def arruda_boyce_energy(C_1,N,Ic):
-	return C_1*(1/2*(Ic - 3)+1/(20*N)*(Ic**2-9)+11/(1050*N**2)*(Ic**3-27)+19/(7000*N**3)*(Ic**4-81)+519/(673750*N**4)*(Ic**5-243))
+def arruda_boyce_energy(mu,lmbda,N,Ic,J,kappa):
+	Ic_dev = Ic*J**(-2/3)
+	psi_dev = mu*(1/2*(Ic - 3)+1/(20*N)*(Ic**2-9)+11/(1050*N**2)*(Ic**3-27)-ln(1+3/(5*N)+99/(175*N**2)))
+	psi_vol = 1/2*lmbda*((J-1) + (ln(J))**2)
+	return (psi_dev + psi_vol)
 
+def neo_hook_strain(F,I,J):
+	B = F*F.T
+	tau = mu*(B-I)
+	return tau/J
+
+u_R = Expression(('0',' 0.5*(0.5 + (x[1] - 0.5)*cos(pi/3) - (x[2] - 0.5)*sin(pi/3) - x[1])','0.5*(0.5 + (x[1] - 0.5)*sin(pi/3) + (x[2] - 0.5)*cos(pi/3) - x[2])'),degree=3)
+#u_R  =Constant((1,0,0))
 bcl = DirichletBC(V, Constant((0, 0, 0)), left)
-bcr = DirichletBC(V, Constant((1, 0, 0)), right)
+bcr = DirichletBC(V,u_R , right)
 bcs = [bcl, bcr]
 
 # Define functions
@@ -37,7 +61,7 @@ du = TrialFunction(V)            # Incremental displacement
 v  = TestFunction(V)             # Test function
 u  = Function(V)                 # Displacement from previous iteration
 B  = Constant((0.0, -0.5, 0.0))  # Body force per unit volume
-T  = Constant((0.3,  0.0, 0.0))  # Traction force on the boundary
+T  = Constant((0.1,  0.0, 0.0))  # Traction force on the boundary
 
 # Kinematics
 d = u.geometric_dimension()
@@ -54,19 +78,32 @@ J  = det(F)
 E, nu = 10.0, 0.3
 N = 8
 C_1 = 2.078
+kappa = Constant(E/(3*(1-2*nu)))
 mu, lmbda = Constant(E/(2*(1 + nu))), Constant(E*nu/((1 + nu)*(1 - 2*nu)))
 
+# stress
+sig_dev = neo_hook_strain(F,I,J) - (1./3)*tr(neo_hook_strain(F,I,J) )*I
+sig_vol = (1./3)*tr(neo_hook_strain(F,I,J) )*I
+
+
+
 # Stored strain energy density (compressible neo-Hookean model)
-#psi = neo_hook(mu,lmbda,Ic,J)
-psi = arruda_boyce_energy(C_1,N,Ic)
+psi = neo_hook_energy(mu,lmbda,Ic,J)
+#psi = arruda_boyce_energy(mu,lmbda,N,Ic,J,kappa)
+
+
 # Total potential energy
 Pi = psi*dx - dot(B, u)*dx - dot(T, u)*ds
+
 
 # Compute first variation of Pi (directional derivative about u in the direction of v)
 F = derivative(Pi, u, v)
 
 # Compute Jacobian of F
 J = derivative(F, u, du)
+test =project(sig_dev,V)
+plot(sig_dev)
+plt.savefig('test.pdf')
 
 # Solve variational problem
 solve(F == 0, u, bcs, J=J,
@@ -76,28 +113,14 @@ solve(F == 0, u, bcs, J=J,
 file = File("displacement.pvd");
 file << u;
 
-# Plot and hold solution
-
-import numpy as np
-# Print errors
-#u_magnitude=np.array(u_magnitude)
-#print('min/max u:',u_magnitude.array().min(),u_magnitude.array().max())
-
-import matplotlib as mpl
-
-plt.rcParams['axes.facecolor']='lightsteelblue'
-#mpl.rcParams['text.usetex'] = True
-#mpl.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}'] 
-plt.rc('lines',linewidth=1,linestyle='solid')
-plt.rc('font',size=18)
-plt.rc('grid',color='w')
-mpl.rcParams['axes.grid'] = True
-mpl.rcParams['xtick.color']='grey'
-mpl.rcParams['ytick.color']='grey'
-plt.rcParams['axes.labelcolor']='darkblue'
-mpl.rcParams["axes.edgecolor"] = 'w'
 
 
+#xdmffile_u = XDMFFile(’navier_stokes_cylinder/velocity.xdmf’)
+#xdmffile_p = XDMFFile(’navier_stokes_cylinder/pressure.xdmf’)
 
-plot(u)
-plt.savefig('displacement.pdf')
+
+#prm = solver.parameters
+#prm['newton_solver']['absolute_tolerance'] = 1E-8
+#prm['newton_solver']['relative_tolerance'] = 1E-7
+#prm['newton_solver']['maximum_iterations'] = 25
+#prm['newton_solver']['relaxation_parameter'] = 1.0
